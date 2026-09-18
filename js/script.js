e7c8caf6acdb7db0f39382d124780894925272b6
Naura567 <nawrazahranii@gmail.com>
2026-09-18 22:02:18 +0700

    p02

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-const promoButton = document.querySelector("#promoButton");
+const promo = document.querySelector("#promo");
 
-promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
-});
+if (promo) {
+  promo.addEventListener("click", () => {
+    promo.textContent = "Promo: Beli 2 Box Gratis 1 Box Mini!";
+  });
+}
